df79a24b27e9a67240e2d17b50428843c7a4fd75
bombr <47834716+bombr90@users.noreply.github.com>
2022-05-31 17:17:05 -0700

    Add GUI functionality and styling
    Removed prompt input and console.log outputs. Replaced inputs with
    buttons and outputs by dynamically updating HTML div elements using
    javascript.

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ The Oden Project Assignment: Rock, Paper, Scissors game
 
 Rock, paper, scissors game implementation using Javascript.
 
-Version 1.0 - implementation in console using 'prompt' for input (no GUI).
+Version 1.0 - implementation in console using 'prompt' for input (no UI).
+
+Version 2.0 (rps-ui) - implementation of a user interface to play RPS
+
+-Replaced console.log outputs by populating a div element.
+-Removed prompt related functions for user input.
+-Removed/refactored functions to take 'click' events instead of prompt inputs.
--- script.js
+++ script.js
@@ -17,12 +17,6 @@ function computerPlay() {
             return 'scissors';
     }
 }
-//Function 'getPlayerSelection' - Prompt player to type input RPS
-function getPlayerSelection()   {   
-    let input = prompt('Enter your selection (rock, paper or scissors)'); //Prompt user for selection
-    let tmp = input.toLowerCase();   //Transform input to lower case
-    return (tmp === 'rock')||(tmp === 'paper')||(tmp === 'scissors') ? tmp : false; //return valid input
-}   
 
 //------------------------
 //Game Functions
@@ -30,12 +24,6 @@ function getPlayerSelection()   {
 //Function 'playRound(playerSelection, computerSelection)' - to play round of RPS
 function playRound(playerSelection, computerSelection) {    
     const sel = [], arr = [];
-    do  {   //Set selections using helper functions. Loop until receive valid input from prompt
-        playerSelection = getPlayerSelection();
-        if(!playerSelection)    {
-            console.log('Invalid input. Please try again \n(type in "rock", "paper" or "scissors").');
-        }
-    }   while(!playerSelection);
     computerSelection = computerPlay();
     if(playerSelection === computerSelection)   {
         arr[1] = 'tie';
@@ -57,25 +45,72 @@ function playRound(playerSelection, computerSelection) {
         arr[0]=`You select ${playerSelection}! ${sel[0]} beats ${sel[1]}`;
     return arr;
     }
-//Function 'game())' - to play multiple games of RPS and declare winner
-function game() {   
-    let playerSelect, computerSelect, playerScore = 0, computerScore = 0, str, arr = [];
-    for (let i = 0; i < 5; i++) {   //Play five rounds of (playRound())
-        arr = playRound(playerSelect, computerSelect);
-        console.log(arr[0]);
-        if(arr[1]==='win')    { //Keep cumulative score 
-            playerScore++;
-        }   else if(arr[1]==='lose') {
-            computerScore++;    
-        }
+
+//------------------------
+//User interface globals and events
+//------------------------
+let playerScore = 0, computerScore = 0;
+const container = document.querySelector('#results');
+
+const btns = document.querySelectorAll('.btns');
+btns.forEach(button => button.addEventListener('click',clickChoice));
+
+function clickChoice(e)    {
+    let playerSelect, computerSelect, arr = [];   
+    switch(e.target.id) {
+        case 'btnRock':
+            playerSelect = 'rock';
+            break;
+        case 'btnPaper':
+            playerSelect = 'paper';
+            break;
+        case 'btnScissors':
+            playerSelect = 'scissors';
+            break;
+        case 'btnReset':
+            resetGame();
+            return
+            break;    
     }
-    str =   (playerScore > computerScore) ? 'Player wins best out of five!' : //Report Winner
-            (playerScore < computerScore) ? 'Computer wins best out of five!' :
-            'Player and Computer Tie!';
-    console.log(`Player Wins: ${playerScore} \nComputer Wins: ${computerScore} \n${str}`);
+    if(playerScore<5 && computerScore<5)    {
+        arr = playRound(playerSelect, computerSelect);
+        writeResult(arr, true);
+        writeResult(updateScore(arr));
+    } else {
+        writeResult('Press "Reset" to play again', true);
+    }
 }
 
 //------------------------
-//Main
-//------------------------
-game();
+//UI Helper Functions
+//------------------------(
+function writeResult(string, clear = false)  {
+    const result = document.createElement('div')
+    result.textContent = string;
+    if(clear)   {
+        container.innerHTML='';
+    }
+    container.append(result);    
+}
+
+function updateScore(arr) {  
+    let maxWins = 5;  
+    if(arr[1]==='win')    {
+        playerScore++;
+    }   else if(arr[1]==='lose') {
+        computerScore++;    
+    }
+    if(playerScore === maxWins || computerScore === maxWins)    {
+        container.classList.add('bold');
+    }
+    return  (playerScore === maxWins) ? `Player wins ${maxWins} rounds, congrats!` :
+            (computerScore === maxWins) ? `Computer wins ${maxWins} rounds, nice try` :
+            `Player Wins: ${playerScore} \nComputer Wins: ${computerScore}`;
+}
+
+function resetGame()    {
+    writeResult('Game Reset', true);
+    playerScore = 0;
+    computerScore = 0; 
+    container.classList.remove('bold');
+}
